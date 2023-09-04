b0c6aaabcb0228272006532b2e56ecbfa7e95415
Jamie Jasmine Koh <jkoh5113@gmail.com>
2023-09-04 12:53:09 +1000

    continuing
    Finished basic home page started basic assets page

--- client/src/pages/Assets.js
+++ client/src/pages/Assets.js
@@ -1,9 +1,24 @@
 import React from 'react'
 
 function Assets() {
+
+  const AssetsList = [{id: 1, asset: "Video", value: 22}, {id: 2, asset: "Images", value: 500}, {id: 3, asset: "Bitcoin", value: 342}];
+
   return (
-    <div>
+    <div className='AssetsPageContainer'>
       <p>testing assets page</p>
+      <div className='assets_list_container'>
+        {AssetsList.map(item => (
+          <div className='assets_list_item'>
+            <div className='assets_name'>
+              <p key={item.id}>{item.asset}</p>
+            </div>
+            <div className='assets_value'>
+              <p key={item.id}>{item.value}</p>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -5,15 +5,22 @@ import person from '../images/person.png';
 function Home() {
   return (
     <div className='HomePageContainer'>
+      <div className='Welcome'>
+        <h1>Welcome To Apex Trades</h1>
+        <p>Meet the team</p>
+      </div>
       <div className='People'>
         <div className='person_images'> 
           <img src={person} alt='avatar'/>
+          <p>Jamie</p>
         </div>
         <div className='person_images'> 
           <img src={person} alt='avatar'/>
+          <p>Liam</p>
         </div>
         <div className='person_images'> 
           <img src={person} alt='avatar'/>
+          <p>Rony</p>
         </div>
         
       </div>
